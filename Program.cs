9ceb470d6720969eea8e5cd8f61c2f48628a43c1
ShoroukShawky <324243558@sha.edu.eg>
2026-02-23 01:34:55 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OOP_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region part01
+            #region Q1
+
+            #endregion
+            #region Q2
+            #endregion
+            #region Q3
+            #endregion
+            #region Q4
+            #endregion
+            #endregion
+            #region part02
+            #endregion
+
+        }
+    }
+}

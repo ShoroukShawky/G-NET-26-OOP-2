14dbdb8345f278fa31d7d687e5249e98ded9c5b8
ShoroukShawky <324243558@sha.edu.eg>
2026-02-23 01:58:19 +0100

    Q2

--- Program.cs
+++ Program.cs
@@ -29,15 +29,47 @@
             #endregion
             #endregion
             #region Q2
-            #endregion
-            #region Q3
-            #endregion
-            #region Q4
-            #endregion
-            #endregion
-            #region part02
-            #endregion
+            /*
+            1- field :
+            - variable declared inside a class
+            - stores data directly
+            - usually private
+            - doesn't contain logic
+             */
 
-        }
+            /*
+             2- property:
+           - provides controlled access to a field
+           - uses get and set
+           - can contain logic -> validation, calculations
+           - supports encapsulation
+             */
+
+            /*
+            3- can a property contain logic?
+            - yes, a property can contain validation or calculations inside get or set
+             */
+
+            // 4- example:
+        //    private double salary;
+        //    private double bonus;
+        //public double TotalIncome
+        //{
+        //    get
+        //    {
+        //        return salary + bonus;
+        //    }
+        //}
+
+        #endregion
+        #region Q3
+        #endregion
+        #region Q4
+        #endregion
+        #endregion
+        #region part02
+        #endregion
+
+    }
     }
 }
